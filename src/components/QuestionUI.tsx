c0488ca0305b96d798dca006d8a05d1394cbfa27
[dyad] <siddh.tanika@gmail.com>
2025-07-31 17:24:06 +0530

    [dyad] Finished refactoring curriculum data - wrote 1 file(s)

--- src/components/QuestionUI.tsx
+++ src/components/QuestionUI.tsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card";
-import { Question } from "@/data/curriculum";
+import { Question } from "@/data/curriculum/types";
 
 interface QuestionUIProps {
   question: Question;
